c3c34db2fb8c66ec0efb2003ea4d35b16be95f0f
Dimitrios Dimitriadis <melkorinos@gmail.com>
2024-05-06 18:59:55 +0200

    better

--- config/config.ts
+++ config/config.ts
@@ -1,63 +1,23 @@
-const resources1 = [
-    {
-        property1: 'value1_1',
-    },
-
-    {
-        property2: 'value2_1',
-    },
-];
-
-const resources2 = [
-    {
-        property1: 'value1_2',
-    },
-
-    {
-        property2: 'value2_2',
-    },
-];
-
 export const config: Record<string, any> = {
-    'some_env': {
+    'example': {
         sets: [
             {
-                resources1: resources1,
-                specificProperty: [
-                    {
-                        property1: 'value1',
-                    },
-                ],
+                propertySet1: 'value1',
             },
             {
-                resources2: resources2,
-                specificProperty: [
-                    {
-                        property1: 'value1',
-                    },
-                ],
+                propertySet2: 'value1',
             },
         ],
-        url: 'https://some_url/',
+        url: 'https://catfact.ninja/',
     },
 
     'some_other_env': {
         sets: [
             {
-                resources1: resources1,
-                specificProperty: [
-                    {
-                        property1: 'value1',
-                    },
-                ],
+                propertySet1: 'value1',
             },
             {
-                resources2: resources2,
-                specificProperty: [
-                    {
-                        property1: 'value1',
-                    },
-                ],
+                propertySet2: 'value1',
             },
         ],
         url: 'https://some_other_url/',
--- config/configHelper.ts
+++ config/configHelper.ts
@@ -2,9 +2,6 @@ import { config } from 'config/config';
 
 let parallelIndex = process.env.TEST_PARALLEL_INDEX;
 
-/**
- * SERVER should be defined in .env file or in the pipeline .yaml for CI/CD
- */
 export function getConfigByServer() {
     const server = process.env.SERVER;
     if (!server) throw new Error('SERVER env variable was not defined');
