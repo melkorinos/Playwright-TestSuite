de789c57d438f196514a756bf15c780bf16ada98
Dimitrios Dimitriadis <melkorinos@gmail.com>
2024-05-08 13:00:17 +0200

    2nd user login

--- e2e/tests/webSite/someTests.test.ts
+++ e2e/tests/webSite/someTests.test.ts
@@ -1,11 +1,19 @@
-import { test, expect } from 'fixtures/fixtures';
+import { test, expect, BrowserFixtures } from 'fixtures/fixtures';
+import { getNewBrowser } from 'helpers/browserHelper';
 
-test('[someID] Login and verify element', async function ({ webComponents, webPages }) {
+let userB: BrowserFixtures;
+
+test('[someID] Login and verify element', async function ({ webComponents, webPages, browser }) {
     await test.step('Login ', async () => {
         await webPages.loginPage.login();
     });
 
     await test.step('Test ', async () => {
         await expect(webComponents.menu.someLocator).toBeEnabled();
+    });
+
+    await test.step('Login another user ', async () => {
+        userB = await getNewBrowser(browser);
+        await userB.webPages.loginPage.login();
     });
 });
