c7880c8a068428f24bc1448f64099f76d5d46bd4
Dimitrios Dimitriadis <melkorinos@gmail.com>
2024-06-14 13:33:15 +0200

    hurray

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -1,7 +1,6 @@
 import { mergeTests } from '@playwright/test';
 
 import { WebComponents, test as webComponents } from './webComponents';
-
 import { WebPages, test as webPages } from './webPages';
 import { test as services } from './services';
 
@@ -13,8 +12,15 @@ import * as customMatchers from '../helpers/customMatchers';
  */
 export type BrowserFixtures = WebPages & WebComponents;
 
+/**
+ * Combine all test components in the test object
+ */
 const test = mergeTests(webPages, webComponents, services);
 
+
+/**
+ * Add custom matchers to expect object
+ */
 const expect = baseExpect.extend({
     toBeHello(received: string) {
         const pass = customMatchers.isHello(received);
