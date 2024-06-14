4a5c3e08f590b8f1a27b5b8a3d683a3c771b943f
Dimitrios Dimitriadis <melkorinos@gmail.com>
2024-06-14 13:31:18 +0200

    update test data

--- testData/someData.ts
+++ testData/someData.ts
@@ -2,10 +2,4 @@ export const someData = {
     emptyString: '',
     whitespace: ' ',
     singleCharacter: 'a',
-    input37chars: 'a'.repeat(37),
-    input201chars: 'a'.repeat(201),
-    input257chars: 'a'.repeat(257),
-
-    invalidGuid: '123',
-    inexistantGuid: '00000000-1111-2222-3333-000000000000',
 };
